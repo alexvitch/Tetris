102db25df74b36e4888b21e0ec28c4f8194cfb79
Alex Vitchinov <52114230+alexvitch@users.noreply.github.com>
2020-05-15 22:33:06 +0300

    Add files via upload
    New pieces has being added and it's coloring

--- src/game.js
+++ src/game.js
@@ -1,68 +1,183 @@
-export default class me {
+export default class Game {
     score = 0;
     lines = 0;
     level = 0;
-    playfield = [
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0]
-    ];
-    activePiece = {
-        x: 0,
-        y: 0,
-        blocks: [
-            [0,1,0],
-            [1,1,1],
-            [0,0,0]
-        ]
-    };
+    playfield = this.createPlayfield();
+    activePiece = this.createPiece();
+    nextPiece = this.createPiece();
+
+    getState() {
+        const playfield = this.createPlayfield();
+        const { y: pieceY, x: pieceX, blocks} = this.activePiece;
+
+        for (let y = 0; y < this.playfield.length; y++) {               //copy playfield state
+            playfield[y] = [];
+
+            for (let x = 0; x < this.playfield[y].length; x++) {
+                playfield[y][x] = this.playfield[y][x];                
+            }
+            
+        }
+
+        for (let y = 0; y < blocks.length; y++) {      // copy blocks state at the same array
+            for (let x = 0; x < blocks[y].length; x++) {
+                if(blocks[y][x]) {
+                    playfield[pieceY + y][pieceX + x] = blocks[y][x]
+                }                
+            }
+            
+        }
+
+        return {
+            playfield
+        };
+    }
+
+    createPlayfield(){
+        const playfield = [];
+
+        for (let y = 0; y < 20; y++) {
+            playfield[y] = [];
+
+            for (let x = 0; x < 10; x++) {
+                playfield[y][x] = 0;                
+            }
+            
+        }
+
+        return playfield;
+    }
+
+    createPiece() {
+        const index = Math.floor(Math.random() * 7);
+        const type = "IJLOSTZ"[index];
+        const piece = {  };
+
+        switch (type) {
+            case "I":
+                piece.blocks = [
+                    [0,0,0,0],
+                    [1,1,1,1],
+                    [0,0,0,0],
+                    [0,0,0,0]
+                ];
+                break;
+            case "J":
+                piece.blocks = [
+                    [0,0,0],
+                    [2,2,2],
+                    [0,0,2]
+                ];
+                break;
+            case "L":
+                piece.blocks = [
+                    [0,0,0],
+                    [3,3,3],
+                    [3,0,0]
+                ];
+                break;
+            case "O":
+                piece.blocks = [
+                    [0,0,0,0],
+                    [0,4,4,0],
+                    [0,4,4,0],
+                    [0,0,0,0]
+                ];
+                break;
+            case "S":
+                piece.blocks = [
+                    [0,0,0],
+                    [0,5,5],
+                    [5,5,0]
+                ];
+                break;
+            case "T":
+                piece.blocks = [
+                    [0,0,0],
+                    [6,6,6],
+                    [0,6,0]
+                ];
+                break;
+            case "Z":
+                piece.blocks = [
+                    [0,0,0],
+                    [7,7,0],
+                    [0,7,7]
+                ];
+                break;
+            default:
+                throw new Error("Unknown type of piece");
+        }
+
+        piece.x = Math.floor((10 - piece.blocks[0].length) / 2);
+        piece.y = -1;
+
+        return piece;
+    }
 
     movePieceLeft() {
         this.activePiece.x -= 1;
 
-        if (this.isOutOfBorder()) {
-            this.activePiece.x += 1;  //isOutOfBorder method usage
+        if (this.hasCollision()) { //hasCollision method usage returning block on previous position on playfield's bounds
+            this.activePiece.x += 1;  
         }
     }
 
     movePieceRight() {
         this.activePiece.x += 1;
 
-        if (this.isOutOfBorder()) {
-            this.activePiece.x -= 1;  //isOutOfBorder method usage
+        if (this.hasCollision()) {
+            this.activePiece.x -= 1;  
         }
     }
 
     movePieceDown() {
         this.activePiece.y += 1;
 
-        if (this.isOutOfBorder()) {
-            this.activePiece.y -= 1;  //isOutOfBorder method usage
+        if (this.hasCollision()) {
+            this.activePiece.y -= 1;  
+            this.pieceLocking();
+            this.updatePieces();
         }
     }
 
-    isOutOfBorder() {
-        const playfield = this.playfield;       //destructing assignment
-        const { y, x } = this.activePiece;
+    rotatePiece() {
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+        const temp = [];
+
+        for (let i = 0; i < length; i++) {
+            temp[i] = new Array(length).fill(0);  //creating new temporary array filling with 0
+        }
+
+        for (let y = 0; y < length; y++) {              
+            for (let x = 0; x < length; x++) {          // calculating new piece position  
+                temp[x][y] = blocks[length - 1 - y][x];  
+            }            
+        }
+
+        this.activePiece.blocks = temp;               // assigning temporary array to block's array
+
+        if(this.hasCollision()) {
+            this.activePiece.blocks = blocks;        //rotating piece in backwards
+        }
+    }
+
+    hasCollision() {
+        const { y: pieceY, x: pieceX, blocks} = this.activePiece;   //transfering blocks properties to the playfield
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (
+                    blocks[y][x] !== 0 && 
+                    ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) || //whether is on border: at first y-line, then y-line + x-line
+                    this.playfield[pieceY + y][pieceX + x])
+                    ) { 
+                    return true;
+                }                
+            }                        
+        }
         
-        return playfield[y] === undefined || playfield[y][x] === undefined; //whether is on border
+        return false; 
     }
 
     pieceLocking() {
@@ -70,8 +185,15 @@ export default class me {
 
         for (let y = 0; y < blocks.length; y++) {
             for (let x = 0; x < blocks[y].length; x++) {
-                this.playfield[pieceY + y][pieceX + x] = blocks[y][x];    //transfering blocks properties to the playfield            
+                if (blocks[y][x]) {
+                    this.playfield[pieceY + y][pieceX + x] = blocks[y][x];  //transfering blocks properties to the playfield 
+                }             
             }                        
         }
+    }
+
+    updatePieces() {
+        this.activePiece = this.nextPiece;
+        this.nextPiece = this.createPiece();
     }
 }
